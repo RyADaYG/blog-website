7cbdc9b8dd2a267be22398012d343200c4724416
RyanDownes <ryanpdownes@gmail.com>
2022-11-14 08:20:55 -0600

    Challenge 7

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get('/contact',(req, res) => {
   res.render('contact',{contactContent: contactContent})
 })
 
+app.get('/compose',(req, res) => {
+  res.render('compose')
+})
+
+app.post('/contact',(req, res) => {
+  console.log("Submit")
+})
+
 
 
 
