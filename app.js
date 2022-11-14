eeb03f801e1f3d7aea5cf7ae9d5eaff2d4c0a53a
RyanDownes <ryanpdownes@gmail.com>
2022-11-14 08:09:24 -0600

    Challenge 5

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.get('/',(req, res) => {
   res.render('home',{homeStartingContent: homeStartingContent})
 })
 
+app.get('/about',(req, res) => {
+  res.render('about',{aboutContent: aboutContent})
+})
+
+app.get('/contact',(req, res) => {
+  res.render('contact',{contactContent: contactContent})
+})
+
 
 
 
