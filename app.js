4e3432ba06ffe9ac3035faa179e75b22574d4553
RyanDownes <ryanpdownes@gmail.com>
2022-11-21 12:01:36 -0600

    PORT Fixes for Heroku

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 
 const app = express();
+const PORT= process.env.PORT || 3000
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
@@ -85,6 +86,6 @@ app.get('/posts/:postName', (req,res) =>{
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log("Server started on port ${PORT}");
 });
